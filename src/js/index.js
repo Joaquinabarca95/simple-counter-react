f9a48546b3594f08f4c1afc97c699a9b5c2fa46d
Joaquinabarca95 <joaquinabarca@live.com>
2021-08-31 15:35:01 +0000

    finished proyect as tutorial

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,55 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import Home from "./component/home.jsx";
+//include fontawesome
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+import PropTypes from "prop-types";
+
+//create your first component
+const SimpleCounter = props => {
+	return (
+		<div className="container">
+			<div className="clock">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="six">{props.digitSix % 10}</div>
+			<div className="five">{props.digitFive % 10}</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
+		</div>
+	);
+};
+
+SimpleCounter.propTypes = {
+	digitOne: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitFour: PropTypes.number,
+	digitFive: PropTypes.number,
+	digitSix: PropTypes.number
+};
+
+let counter = 0;
+setInterval(function() {
+	let one = Math.floor(counter / 1);
+	let two = Math.floor(counter / 10);
+	let three = Math.floor(counter / 100);
+	let four = Math.floor(counter / 1000);
+	let five = Math.floor(counter / 10000);
+	let six = Math.floor(counter / 100000);
+	counter++;
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+			digitFive={five}
+			digitSix={six}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
